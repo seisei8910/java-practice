3c913a8363872936c89de8b24c7f3b2e5ea13146
Seisei.Utaka <185202718+seisei8910@users.noreply.github.com>
2025-06-13 08:04:09 +0900

    javaの基本文法

--- src/Ready02.java
+++ src/Ready02.java
@@ -3,7 +3,8 @@ public class Ready02 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
 	}
 
 }
