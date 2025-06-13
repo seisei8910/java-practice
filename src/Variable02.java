622079d94b12e5b524d72e8d5070ce01292d1ebc
Seisei.Utaka <185202718+seisei8910@users.noreply.github.com>
2025-06-13 09:14:57 +0900

    javaの定数

---
+++ src/Variable02.java
@@ -0,0 +1,15 @@
+
+public class Variable02 {
+	
+	static final int MIN_NUMBER = 10;
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        System.out.println(MIN_NUMBER);
+
+        final int maxValue;
+        maxValue = 100;
+        System.out.println(maxValue);
+	}
+
+}
