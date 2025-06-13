0325e69b446ba87686a77899421ea89ec8fe218f
Seisei.Utaka <185202718+seisei8910@users.noreply.github.com>
2025-06-13 10:17:36 +0900

    型の基本 確認問題

---
+++ src/Chapter06.java
@@ -0,0 +1,28 @@
+
+public class Chapter06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		long long1 = 100000000000L;
+		System.out.println(long1);
+		
+		double pi = 3.14d;
+		System.out.println(pi);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Integer integer1 = Integer.valueOf(100);
+		System.out.println(integer1);
+		
+		Float float1 = Float.valueOf(1.4142f);
+		System.out.println(float1);
+		
+		Character character1 = Character.valueOf('う');
+		System.out.println(character1);
+		
+		int[] array1 = new int[] { 0, 10, 20, 30, 40 };
+		System.out.println(array1[3]);
+	}
+
+}
