9fc884b1697f8e9d980752d9bd867e2f13e898f0
Seisei.Utaka <185202718+seisei8910@users.noreply.github.com>
2025-06-13 09:24:28 +0900

    変数確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,16 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "Seisei";
+		System.out.println(name);
+		
+		name = "ご飯";
+		System.out.println(name);
+		
+		final int luckyNum = 9;
+		System.out.println(luckyNum);
+	}
+
+}
